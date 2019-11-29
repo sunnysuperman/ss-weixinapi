81b5640715162397cd9dd2692f522936f2151fae
Leo Ji <jirenshi@congzhitech.com>
2019-11-29 09:38:05 +0800

    Add clearQuota api

--- src/main/java/com/sunnysuperman/weixinapi/token/WeixinTokenApi.java
+++ src/main/java/com/sunnysuperman/weixinapi/token/WeixinTokenApi.java
@@ -3,6 +3,7 @@ package com.sunnysuperman.weixinapi.token;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.sunnysuperman.weixinapi.BaseResponse;
 import com.sunnysuperman.weixinapi.WeixinApi;
 import com.sunnysuperman.weixinapi.WeixinApp;
 import com.sunnysuperman.weixinapi.exception.WeixinApiException;
@@ -30,6 +31,12 @@ public class WeixinTokenApi extends WeixinApi {
 
     public GetTicketResponse getJsTicket(String accessToken) throws WeixinApiException {
         return getTicket(accessToken, "jsapi");
+    }
+
+    public void clearQuota(String accessToken) throws WeixinApiException {
+        Map<String, Object> params = new HashMap<>();
+        params.put("appid", app.getAppId());
+        postJSON("cgi-bin/clear_quota?access_token=" + accessToken, params, new BaseResponse());
     }
 
 }
--- src/test/java/com/sunnysuperman/weixinapi/test/TokenApiTest.java
+++ src/test/java/com/sunnysuperman/weixinapi/test/TokenApiTest.java
@@ -14,4 +14,10 @@ public class TokenApiTest extends BaseTest {
         System.out.println(response.getAccess_token());
         System.out.println(response.getExpires_in());
     }
+
+    public void test_clearQuota() throws Exception {
+        WeixinApp app = new WeixinApp("", "", WeixinAppType.mp);
+        WeixinTokenApi api = new WeixinTokenApi(app);
+        api.clearQuota("");
+    }
 }
