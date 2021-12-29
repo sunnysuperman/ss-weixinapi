d4ae0ff1ec72fd905375b5e3554c4893eba89ef4
zhanghandong@congzhitech.com <dtRrPWvS48KTTB4>
2021-12-29 15:59:13 +0800

    小商店上传图片

---
+++ src/main/java/com/sunnysuperman/weixinapi/shop/model/ImgInfo.java
@@ -0,0 +1,23 @@
+package com.sunnysuperman.weixinapi.shop.model;
+
+public class ImgInfo {
+
+    private String media_id;
+    private String temp_img_url;
+
+    public String getMedia_id() {
+        return media_id;
+    }
+
+    public void setMedia_id(String media_id) {
+        this.media_id = media_id;
+    }
+
+    public String getTemp_img_url() {
+        return temp_img_url;
+    }
+
+    public void setTemp_img_url(String temp_img_url) {
+        this.temp_img_url = temp_img_url;
+    }
+}
